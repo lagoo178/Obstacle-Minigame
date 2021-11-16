f72c4e9f46b595de7c14d8c4a3038432e260bb2c
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-11-16 23:03:04 +0700

    OnCollisionEnter

---
+++ Obstacle Minigame/Assets/ObjectHit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) 
+    {
+        Debug.Log("You Hit The Wall!");
+    }
+}
