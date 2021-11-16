b17d36ea2fd1c653d7df944875f0fd2784090004
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-11-16 22:51:18 +0700

    Cleaner Code

--- Obstacle Minigame/Assets/Mover.cs
+++ Obstacle Minigame/Assets/Mover.cs
@@ -9,12 +9,22 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstruction(){
+        Debug.Log("Welcome to the Obstacle Minigame");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!!");
+    }
+
+    void MovePlayer(){
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
